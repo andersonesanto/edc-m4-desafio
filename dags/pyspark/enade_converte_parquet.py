80ef2dce1b5e9acabbb1ce198f3d1f099d133509
Anderson Santo <andersonesanto@gmail.com>
2021-09-26 15:13:35 -0300

    v0.5

--- dags/pyspark/enade_converte_parquet.py
+++ dags/pyspark/enade_converte_parquet.py
@@ -1,13 +1,17 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
+import os
 
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 # set conf
 conf = (
 SparkConf()
-    .set("spark.hadoop.fs.s3a.fast.upload", True)
-    .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    .set('spark.hadoop.fs.s3a.aws.credentials.provider', 'com.amazonaws.auth.EnvironmentVariableCredentialsProvider')
+#    .set("spark.hadoop.fs.s3a.fast.upload", True)
+#    .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     .set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.3')
+#    .set("spark.hadoop.fs.s3a.access.key", aws_access_key_id)
+#    .set("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key)
 )
 
 # apply config
@@ -29,7 +33,7 @@ if __name__ == "__main__":
         .read
         .format("csv")
         .options(header='true', inferSchema='true', delimiter=';')
-        .load("s3a://m4-597495568095/landing-zone/enade/MICRODADOS_ENADE_2017.txt")
+        .load("s3://m4-597495568095/landing-zone/enade/MICRODADOS_ENADE_2017.txt")
     )
     
     df.printSchema()
@@ -38,7 +42,7 @@ if __name__ == "__main__":
     .write
     .mode("overwrite")
     .format("parquet")
-    .save("s3a://m4-597495568095/processing-zone/enade/")
+    .save("s3://m4-597495568095/processing-zone/enade/")
     )
 
     print("*********************")
